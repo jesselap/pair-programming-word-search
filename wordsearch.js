a5cfb9bf3e4e6ef7aa622c88649852db0d0360d8
Jesse <jesse.a.lapointe@gmail.com>
2021-05-05 21:17:37 +0000

    did one half of diagonal

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,51 @@
 const wordSearch = (letters, word) => { 
+    const result = [];
+    const result2 = [];
+    const reverseWord = word.split('').reverse().join('');
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+         } else if (l.includes(reverseWord)) {
+             return true;
+         }
+    }
+    for (let i = 0; i < letters.length; i++) {
+        let tempArr = [];
+        for (let j = 0; j < letters[i].length; j++) {
+            tempArr.push(letters[j][i]);
+        }
+        result.push(tempArr);
+    }
+    const verticalJoin = result.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true
+     } else if (l.includes(reverseWord)) {
+         return true;
+     }
+ }
+for (let i = 0; i < letters.length; i++){
+    let tempArr =[]
+    for (let j = 0; j <= i; j++) {
+       let k = i - j;
+       tempArr.push(letters[k][j])
+    }
+    result2.push(tempArr);
+}
+
+
+const diagonalJoin = result2.map(ls => ls.join(''))
+for (l of diagonalJoin) {
+    if (l.includes(word)) {
+        return true;
+    } else if (l.includes(reverseWord)) {
+        return true;
     }
 }
+
+  return false;
+}
+
 
 module.exports = wordSearch
